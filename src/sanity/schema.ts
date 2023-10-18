62a6a4efa5d30b62c65ee2b86d5eb6a4a1e3df4a
Muhammad Rafi Ali <therafiali@gmail.com>
2023-10-18 20:33:49 +0500

    start again

--- src/sanity/schema.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import job from "./job";
-import profile from "./profile";
-import project from "./project";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, job, project],
-}
